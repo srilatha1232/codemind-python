7c88a4ae262ac74db2f111ca088dae97737c556b
srilatha1232 <98728683+srilatha1232@users.noreply.github.com>
2023-06-09 10:37:01 +0530

    a=int(input()) while(a):     b=int(input())     s=list(map(int,input().split()))     b=sorted(s)     if(s==b):         print(0)     else:         print(max(b)-min(b))     a-=1

---
+++ Optimal_Sorting.py
@@ -0,0 +1,12 @@
+
+a=int(input())
+while(a):
+    b=int(input())
+    s=list(map(int,input().split()))
+    b=sorted(s)
+    if(s==b):
+        print(0)
+    else:
+        print(max(b)-min(b))
+    a-=1    
+
