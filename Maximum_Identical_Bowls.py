76b571d509dc3d7c8789757b3b2e5987c0870faa
srilatha1232 <98728683+srilatha1232@users.noreply.github.com>
2023-06-09 10:32:32 +0530

    def identical(l,s,n):     for i in range(n,0,-1):         if(s%i==0):             return i     else:             return 0
    n=int(input())
    l=list(map(int,input().split()))
    s=sum(l)
    print(identical(l,s,n))

---
+++ Maximum_Identical_Bowls.py
@@ -0,0 +1,13 @@
+
+def identical(l,s,n):
+    for i in range(n,0,-1):
+        if(s%i==0):
+            return i
+    else:
+            return 0
+            
+n=int(input())
+l=list(map(int,input().split()))
+s=sum(l)
+print(identical(l,s,n))
+
